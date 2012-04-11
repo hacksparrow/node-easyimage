2176e886f679567850b2ab09abcf2c242a2b12f2
Hack Sparrow <captain@hacksparrow.com>
2012-04-11 12:52:30 +0530

    Better test code. Test for thumbnail().

--- test.js
+++ test.js
@@ -1,25 +1,28 @@
 var easyimg = require('easyimage');
 
-easyimg.info('sample-images/cute kitten.jpg', function(err, stdout, stderr) {
+var srcimg = 'sample-images/thed.jpg';
+
+easyimg.info(srcimg, function(err, stdout, stderr) {
 	if (err) throw err;
 	console.log(stdout);
 });
 
 
-easyimg.convert({src:'sample-images/cute kitten.jpg', dst:'cute kitten.png', quality:10}, function(err, stdout, stderr) {
+easyimg.convert({src:srcimg, dst:'convert.png', quality:10}, function(err, stdout, stderr) {
 	if (err) throw err;
 	console.log('Converted');
 });
 
 
-easyimg.resize({src:'sample-images/cute kitten.jpg', dst:'kitten-small.jpg', width:640, height:480}, function(err, stdout, stderr) {
+easyimg.resize({src:srcimg, dst:'resize.jpg', width:640, height:480}, function(err, stdout, stderr) {
 	if (err) throw err;
 	console.log('Resized');
 });
 
+
 easyimg.crop(
 	{
-		src:'sample-images/cute kitten.jpg', dst:'cute kitten.jpg',
+		src:srcimg, dst:'crop.jpg',
 		cropwidth:128, cropheight:128,
 		gravity:'North',
 		x:0, y:0
@@ -30,9 +33,21 @@ easyimg.crop(
 	}
 );
 
+easyimg.thumbnail(
+	{
+		src:srcimg, dst:'thumbnail.jpg',
+		width:128, height:128,
+		x:0, y:0
+	},
+	function(err, stdout, stderr) {
+		if (err) throw err;
+		console.log('Thumbnail created');
+	}
+);
+
 easyimg.rescrop(
 	{
-		src:'sample-images/cute kitten.jpg', dst:'kitten-thumbnail.jpg',
+		src:srcimg, dst:'rescrop.jpg',
 		width:500, height:500,
 		cropwidth:128, cropheight:128,
 		x:0, y:0
@@ -43,7 +58,7 @@ easyimg.rescrop(
 	}
 );
 
-easyimg.exec('convert sample-images/cute\\ kitten.jpg cute\\ kitten.gif', function(err, stdout, stderr) {
+easyimg.exec('convert '+ srcimg +' command.gif', function(err, stdout, stderr) {
 	if (err) throw err;
 	console.log('Command executed');
 });
