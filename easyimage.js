c393bb16b87741fa1e17cd6295133cc9e7e5ff56
Dylan McDiarmid <dylanmcd@gmail.com>
2012-05-19 08:05:33 -0700

    Merged upstream changes

--- easyimage.js
+++ easyimage.js
@@ -4,7 +4,7 @@ var imcmd; // short for ImageMagick Command, how ingenious (y)
 
 // treasure of error messages
 var error_messages = {
-	'path': 'Missing image paths.\nMake sure both source and destination files are specified.',	
+	'path': 'Missing image paths.\nMake sure both source and destination files are specified.',
 	'dim': 'Missing dimensions.\nSpecify the width atleast.',
 	'restricted': 'The command you are trying to execute is prohibited.',
 	'unsupported': 'File not supported.',
@@ -72,7 +72,7 @@ exports.convert = function(options, callback) {
 // resize an image
 exports.resize = function(options, callback) {
 	if (options.src === undefined || options.dst === undefined) throw_err('path');
-	if (options.width === undefined) throw_err('dim'); 
+	if (options.width === undefined) throw_err('dim');
 	options.height = options.height || options.width;
 	options.src = quoted_name(options.src);
 	options.dst = quoted_name(options.dst);
@@ -87,14 +87,14 @@ exports.resize = function(options, callback) {
 // crop an image
 exports.crop = function(options, callback) {
 	if (options.src === undefined || options.dst === undefined) throw_err('path');
-	if (options.cropwidth === undefined) throw_err('dim'); 
+	if (options.cropwidth === undefined) throw_err('dim');
 	options.cropheight = options.cropheight || options.cropwidth;
 	options.gravity = options.gravity || 'Center';
 	options.x = options.x || 0;
 	options.y = options.y || 0;
 	options.src = quoted_name(options.src);
 	options.dst = quoted_name(options.dst);
-	
+
 	if (options.quality === undefined) imcmd = 'convert ' + options.src + ' -gravity ' + options.gravity + ' -crop '+ options.cropwidth + 'x'+ options.cropheight + '+' + options.x + '+' + options.y + ' ' + options.dst;
 	else  imcmd = 'convert ' + options.src + ' -gravity ' + options.gravity + ' -crop '+ options.cropwidth + 'x'+ options.cropheight + '+' + options.x + '+' + options.y + ' -quality ' + options.quality + ' ' + options.dst;
 	child = exec(imcmd, function(err, stdout, stderr) {
@@ -107,19 +107,20 @@ exports.crop = function(options, callback) {
 // resize and crop in one shot!
 exports.rescrop = function(options, callback) {
 	if (options.src === undefined || options.dst === undefined) throw_err('path');
-	if (options.width === undefined) throw_err('dim'); 
+	if (options.width === undefined) throw_err('dim');
 	options.height = options.height || options.width;
-	
-	options.cropwidth == options.cropwidth || options.width;
-	options.cropheight == options.cropheight || options.height;
-	
+
+	options.cropwidth = options.cropwidth || options.width;
+	options.cropheight = options.cropheight || options.height;
+
 	options.gravity = options.gravity || 'Center';
 	options.x = options.x || 0;
 	options.y = options.y || 0;
 	options.src = quoted_name(options.src);
 	options.dst = quoted_name(options.dst);
-	if (options.quality === undefined) imcmd = 'convert ' + options.src + ' -resize ' + options.width + 'x' + options.height + ' -gravity ' + options.gravity + ' -crop '+ options.cropwidth + 'x'+ options.cropheight + '+' + options.x + '+' + options.y + ' ' + options.dst;
-	else imcmd = 'convert ' + options.src + ' -resize ' + options.width + 'x' + options.height + ' -gravity ' + options.gravity + ' -crop '+ options.cropwidth + 'x'+ options.cropheight + '+' + options.x + '+' + options.y + ' -quality ' + options.quality + ' ' + options.dst;
+	options.fill = options.fill ? '^' : '';
+	if (options.quality === undefined) imcmd = 'convert ' + options.src + ' -resize ' + options.width + 'x' + options.height + options.fill + ' -gravity ' + options.gravity + ' -crop '+ options.cropwidth + 'x'+ options.cropheight + '+' + options.x + '+' + options.y + ' ' + options.dst;
+	else imcmd = 'convert ' + options.src + ' -resize ' + options.width + 'x' + options.height + options.fill + ' -gravity ' + options.gravity + ' -crop '+ options.cropwidth + 'x'+ options.cropheight + '+' + options.x + '+' + options.y + ' -quality ' + options.quality + ' ' + options.dst;
 	child = exec(imcmd, function(err, stdout, stderr) {
 		if (err) throw err;
 		info(options.dst, callback);
@@ -128,33 +129,33 @@ exports.rescrop = function(options, callback) {
 
 // create thumbnails
 exports.thumbnail = function(options, callback) {
-	
+
 	if (options.src === undefined || options.dst === undefined) throw_err('path');
-	if (options.width === undefined) throw_err('dim'); 
+	if (options.width === undefined) throw_err('dim');
 	options.height = options.height || options.width;
 	options.gravity = options.gravity || 'Center';
 	options.x = options.x || 0;
 	options.y = options.y || 0;
 	options.src = quoted_name(options.src);
 	options.dst = quoted_name(options.dst);
-	
+
 	info(options.src, function(err, original, stderr) {
 		if (err) throw err;
-		
+
 		// dimensions come as strings, convert them to number
 		original.width = +original.width;
 		original.height = +original.height;
-		
+
 		var resizewidth = options.width;
 		var resizeheight = options.height;
-		
+
 		if (original.width > original.height) { resizewidth = ''; }
 		else if (original.height > original.width) { resizeheight = ''; }
-		
+
 		// resize and crop
 		if (options.quality === undefined) imcmd = 'convert ' + options.src + ' -resize ' + resizewidth + 'x' + resizeheight + ' -gravity ' + options.gravity + ' -crop '+ options.width + 'x'+ options.height + '+' + options.x + '+' + options.y + ' ' + options.dst;
 		else imcmd = 'convert ' + options.src + ' -resize '+ resizewidth + 'x' + resizeheight + ' -quality ' + options.quality + ' -gravity ' + options.gravity + ' -crop '+ options.width + 'x'+ options.height + '+' + options.x + '+' + options.y + ' -quality ' + options.quality + ' ' + options.dst;
-		
+
 		child = exec(imcmd, function(err, stdout, stderr) {
 			if (err) throw err;
 			info(options.dst, callback);
@@ -168,7 +169,7 @@ exports.exec = function(command, callback) {
 	var _command = command.split(' ')[0];
 	// as a security measure, we will allow only 'convert' commands
 	if (_command != 'convert') throw_error('restricted');
-	
+
 	child = exec(command, function(err, stdout, stderr) { callback(err, stdout, stderr); });
 };
 
